855ed834ca246a4c04f8d638f294321dbdf204ae
Guillaume Broggi <25569517+GuillaumeBroggi@users.noreply.github.com>
2025-01-29 16:39:10 +0100

    Fix test

--- tests/data/test_graph_data.py
+++ tests/data/test_graph_data.py
@@ -33,11 +33,11 @@ def test_graph_data_set_graph_edges_indexes_unique(graph_data):
     edge_indices = np.random.randint(0, graph_data.get_n_node(), (10, 2))
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Set the edge indices
-    unique_indices = graph_data.set_graph_edges_indexes(
-                                    edges_indexes_mesh=edge_indices)
+    graph_data.set_graph_edges_indexes(edges_indexes_mesh=edge_indices)
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Assert edge indices are unique
-    _, unique_counts = np.unique(unique_indices, return_counts=True)
+    _, unique_counts = np.unique(graph_data.get_graph_edges_indexes(), axis=0,
+                                 return_counts=True)
     assert (unique_counts == 1).all()
 # -----------------------------------------------------------------------------
 def test_graph_data_set_graph_edges_indexes_non_unique(graph_data):
