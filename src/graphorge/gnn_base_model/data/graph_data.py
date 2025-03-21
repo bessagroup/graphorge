82d6f76f0efd204b0550a160c8123f699c271af5
RBMP <rbarreira@ethz.ch>
2025-03-21 23:54:51 +0100

    graph data clean

--- src/graphorge/gnn_base_model/data/graph_data.py
+++ src/graphorge/gnn_base_model/data/graph_data.py
@@ -167,14 +167,6 @@ class GraphData:
         if self._node_features_matrix is not None:
             x = torch.tensor(copy.deepcopy(self._node_features_matrix),
                              dtype=torch.float)
-            # test time dimension: nodal features
-            # print('x.shape no time dim: ', x.shape)
-            x_new = torch.zeros((20, *x.shape), dtype=torch.float)
-            for i in range(20):
-                x_new[i, :, :] = x * (1 - 0.05 * i)
-            # print('x.shape with time dim: ', x_new.shape) 
-            x = x_new.view(x.shape[0], x.shape[1]*20)
-            # print('x.reshape with time dim: ', x.shape)
         # Set PyG graph connectivity
         edge_index = None
         if self._edges_indexes is not None:
@@ -186,12 +178,6 @@ class GraphData:
         if self._edge_features_matrix is not None:
             edge_attr = torch.tensor(copy.deepcopy(self._edge_features_matrix),
                                      dtype=torch.float)
-            # # # test time dimension: edge features
-            # edge_attr_new = torch.zeros((20, *edge_attr.shape), dtype=torch.float)
-            # for i in range(20):
-            #     edge_attr_new[i, :, :] = edge_attr * (1 - 0.05 * i)
-            # edge_attr = edge_attr_new.view(edge_attr.shape[0], edge_attr.shape[1]*20)
-        # Set PyG node ground-truth labels
         y = None
         if self._node_targets_matrix is not None:
             y = torch.tensor(copy.deepcopy(self._node_targets_matrix),
@@ -215,15 +201,6 @@ class GraphData:
             global_features_matrix = \
                 torch.tensor(copy.deepcopy(self._global_features_matrix),
                              dtype=torch.float)
-            # # test time dimension: global features
-            # global_attr_new = torch.zeros((20, *global_features_matrix.shape),
-            #                                dtype=torch.float)
-            # for i in range(20):
-            #     global_attr_new[i, :, :] = global_features_matrix * (
-            #                                                 1 - 0.05 * i)
-            # global_features_matrix = global_attr_new.view(
-            #                         global_features_matrix.shape[0],
-            #                         global_features_matrix.shape[1]*20)
         # Set global ground-truth labels
         global_targets_matrix = None
         if self._global_targets_matrix is not None:
