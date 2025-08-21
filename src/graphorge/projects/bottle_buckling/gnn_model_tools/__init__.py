be00d4586b42f80b0b88ff668e53392e37df81d6
Guillaume Broggi <25569517+GuillaumeBroggi@users.noreply.github.com>
2025-08-21 18:38:28 +0200

    Remove old projects

--- src/graphorge/projects/bottle_buckling/gnn_model_tools/__init__.py
+++
@@ -1,6 +0,0 @@
-#
-#                                                                       Modules
-# =============================================================================
-from projects.bottle_buckling.gnn_model_tools import features
-from projects.bottle_buckling.gnn_model_tools import gen_graphs_files
-from projects.bottle_buckling.gnn_model_tools import process_predictions
